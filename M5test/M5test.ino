6dc9d031c765771c5c498db9434d57106746caac
flamerten <flamerten@gmail.com>
2022-08-08 16:37:07 +0800

    New attempt at callibration
    Im trying to use the maximum reading or after a period of time. Because it appears that the true reading takes a while due to the nature of the gradient descent

--- M5test/M5test.ino
+++ M5test/M5test.ino
@@ -1,11 +1,15 @@
 //Board is M5 Atom Stack
 //Referenced from https://docs.m5stack.com/en/api/atom/mpu
+//Orientation Filter based on https://courses.cs.washington.edu/courses/cse466/14au/labs/l4/madgwick_internal_report.pdf
+
 
 #include <Adafruit_NeoPixel.h> //This is for the onboard LEDs
 #include <Wire.h> //I2c for the MPU
 #include "M5Atom.h" //helper functions for the MPU
 
 float pitch, roll, yaw; //Stores attitude related variables.
+
+#define READ_DELAY  100 //magdwick filter is effective at low sampling rates
 float cal_pitch, cal_roll; //calibration during init;
 
 //On board LED matrix
@@ -35,13 +39,25 @@ void calibrate_IMU(){
   cal_pitch = 0;
   cal_roll = 0;
 
-  int num_iter = 200;
+  float max_pitch, max_roll;
+  float min_pitch, min_roll;
+
+  max_pitch = max_roll = -200;
+  min_pitch = min_roll = 200;
+
+  int num_iter = 50;
 
   for(int i = 0; i < num_iter; i++){
     M5.IMU.getAhrsData(&pitch,&roll,&yaw);
+    
+    if(pitch > max_pitch) max_pitch = pitch;
+    if(pitch < min_pitch) min_pitch = pitch;
+    if(roll > max_roll) max_roll = roll;
+    if(roll < min_roll) min_roll = roll;
+    
     cal_pitch += pitch;
     cal_roll += roll;
-    delay(50);
+    delay(READ_DELAY);
   }
 
   cal_pitch = cal_pitch/num_iter;
@@ -51,6 +67,34 @@ void calibrate_IMU(){
   light_middle(0,100,0); //light up green when finished
   Serial.printf("cal_pitch:%.2f, cal_roll:%.2f", cal_pitch,cal_roll);
   Serial.println();
+  Serial.printf(
+    "PITCH Variation:%.2f , Max_pitch:%.2f, Min_Pitch: %2f",
+    max_pitch - min_pitch,
+    max_pitch,
+    min_pitch
+    );
+    
+  Serial.println();
+  Serial.printf(
+    "ROLL Variation:%.2f , Max_roll:%.2f, Min_roll: %2f",
+    max_roll - min_roll,
+    max_roll,
+    min_roll
+    );
+
+   Serial.println();
+   
+}
+
+
+void calibrate_IMU_MAX(){
+  Serial.println("Calibrating");
+  light_middle(100,0,0);//light up red during calibration
+  cal_pitch = 0;
+  cal_roll = 0;
+
+
+  light_middle(0,100,0); //light up green when finished
   
 }
 
@@ -60,7 +104,7 @@ void setup() {
   init_matrix();
   M5.IMU.Init();
 
-  calibrate_IMU();
+  //calibrate_IMU();
 
 }
 
@@ -73,6 +117,13 @@ void loop() {
   roll = roll - cal_roll;
   Serial.printf("CALIBRATED pitch:%.2f, roll:%.2f", pitch,roll);
   Serial.println();
-  delay(100);
+
+  //For The serial plotter printout
+  /*Serial.print(pitch);
+  Serial.print(" ");
+  Serial.print(roll);
+  Serial.println();*/
+  
+  delay(READ_DELAY);
 
 }
